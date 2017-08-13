0ff72ee29b58a2a051bf18df02f1bc9ec0edde5d
Mischa Rouleaux <mischa-rouleaux@live.nl>
2017-08-13 13:48:07 +0200

    coder-swag update 3

---
+++ coder-swag/coder-swag/Views/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCellCollectionViewCell.swift
+//  coder-swag
+//
+//  Created by Mischa Rouleaux on 13-08-17.
+//  Copyright © 2017 Mischa Rouleaux. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
